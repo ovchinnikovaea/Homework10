6add99939fb458a54ffc0a2febcfff051e6a5e49
ovchinnikova <ov4innikova.e.a@mail.ru>
2023-08-28 21:14:05 +0300

    homework done

--- src/Main.java
+++ src/Main.java
@@ -2,15 +2,28 @@ public class Main {
     public static void main(String[] args) {
         task1();
         task2();
+        task3();
     }
 
     public static void task1 () {
         System.out.println("Задача 1");
-        // Пишем код для задачи 1
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName;
+        fullName = lastName + " " + firstName + " " + middleName;
+        System.out.println("ФИО сотрудника - " + fullName);
     }
-
     public static void task2 () {
         System.out.println("Задача 2");
-        // Пишем код для задачи 2
+        String fullName = "Ivanov Ivan Ivanovich";
+        System.out.println("Данные ФИО сотрудника для заполнения отчета - " + fullName.toUpperCase());
+    }
+    public static void task3 () {
+        System.out.println("Задача 3");
+        String fullName = "Иванов Семён Семёнович";
+        fullName = fullName.replace("ё", "e");
+        fullName = fullName.replace("Ё", "Е");
+        System.out.println("Данные ФИО сотрудника для заполнения отчета - " + fullName);
     }
 }
